0b940555b3a8885fbc2abe87d1d3da7e840d2043
jhoniercalero <jhonier.calero@correounivalle.edu.co>
2018-10-13 20:21:31 -0500

    Se agrega funcionalidad para activar y desactivar los cargos registrados

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\MMV Lab\Desktop\Proyecto-SI\ERPAdmin-1\venv"
+set "VIRTUAL_ENV=C:\Users\caror\Desktop\ERPAdmin\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
