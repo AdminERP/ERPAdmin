9bc69e4d195fc1bef5a40c387296e24404a3cc74
Carlos Tovar <carlos.fernando.tovar@correounivalle.edu.co>
2019-02-14 12:25:06 -0500

    crear orden servicio added

--- apps/ordenes_servicio/models.py
+++ apps/ordenes_servicio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class OrdenServicio(models.Model):
+    servicio_vendido = models.CharField(max_length=100) # Datos Mestros
+    encargado = models.CharField(max_length=100) # Datos Maestros
+    cliente = models.CharField(max_length=100) # Datos Maestros
+    comentarios = models.CharField(max_length=255)
+
+    # Constantes para OrdenServicio
+    ASIGNADA = 'AS'
+    TRAMITE = 'TR'
+    CANCELADA = 'CA'
+    CERRADA = 'CE'
+    # Fin Constantes
+
+    opciones_estado = (
+        (ASIGNADA, 'Asignada'),
+        (TRAMITE, 'Tramite'),
+        (CANCELADA, 'Cancelada'),
+        (CERRADA, 'Cerrada'),
+    )
+    estado = models.CharField(
+        max_length=2,
+        choices=opciones_estado,
+        default=ASIGNADA,
+    )
