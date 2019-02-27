3034317b123bebdd23d1b97d08e5964b628a907f
miguelpaz418 <paz.luis@correounivalle.edu.co>
2019-02-26 22:58:56 -0500

    creacion de los modelos

--- apps/cuentas/models.py
+++ apps/cuentas/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class CuentaEmpresa(models.Model):
+    saldo = models.IntegerField()    
+
+
+class CuentaPagar(models.Model):
+    total = models.IntegerField()
+    numero_orden = models.IntegerField()
+    fecha_factura = models.DateTimeField()
+    plazo = models.DateTimeField()
+    estado = models.BooleanField()
+    proveedor = models.CharField(max_length=30)
+    cuenta_empresa = models.ForeignKey(CuentaEmpresa, on_delete=models.CASCADE)
+
+
+
+class Item(models.Model):
+    cuenta_por_pagar = models.ForeignKey(CuentaPagar, on_delete=models.CASCADE)
+    detalle = models.CharField(max_length=30)
+    precio_total = models.IntegerField()
+
+class CuentaCobrar(models.Model):
+    tarifa = models.IntegerField()
+    costo_total = models.IntegerField()
+    Fecha_vencimiento = models.DateTimeField()
+    estado = models.BooleanField()
+    servicio = models.CharField(max_length=50)
+    cuenta_empresa = models.ForeignKey(CuentaEmpresa, on_delete=models.CASCADE)
