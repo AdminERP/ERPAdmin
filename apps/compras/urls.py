fbea6978f805bee034f6ae84ab57f13b8b431bba
melissamillares <diana.millares@correounivalle.edu.co>
2019-03-06 09:02:58 -0500

    primeros templates

--- apps/compras/urls.py
+++ apps/compras/urls.py
@@ -3,10 +3,12 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+from .views import ListarSolicitudes
+
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='base.html'))
+    path('',TemplateView.as_view(template_name='base.html')),
     # path('solicitud/crear', ,name='solicitud_crear'),
-    # path('solicitud/listar', ,name='solicitud_listar'),
+    path('solicitud/listar', ListarSolicitudes.as_view(), name='solicitud_listar'),
     # path('solicitud/detalle/<int:pk>', ,name='solicitud_detalles'),
     # 
     # path('orden/crear', ,name='orden_crear'),
--- apps/compras/views.py
+++ apps/compras/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import SolicitudCompra
 
 """ Views modulo Compras """
+
+class ListarSolicitudes(ListView):
+    model = SolicitudCompra
+    template_name = "general.html"
