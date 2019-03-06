11c6fed8687a5a3fe435a0b5575b263a8eb620b0
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-03-05 19:16:27 -0500

    arreglar errores iniciales

--- ERPAdmin/urls.py
+++ ERPAdmin/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', TemplateView.as_view(template_name='base.html')), #para renderizar la plantilla base
-    path('compras/',include(('usuarios.urls','compras'),namespace='compras')),
+    path('compras/',include(('apps.compras.urls','compras'),namespace='compras')),
 ]
--- apps/compras/urls.py
+++ apps/compras/urls.py
@@ -1,13 +1,15 @@
 """ERPAdmin Compras URL Configuration """
 
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('solicitud/crear', ,name='solicitud_crear'),
-    path('solicitud/listar', ,name='solicitud_listar'),
-    path('solicitud/detalle/<int:pk>', ,name='solicitud_detalles'),
-    
-    path('orden/crear', ,name='orden_crear'),
-    path('orden/listar', ,name='orden_listar'),
-    path('orden/detalle/<int:pk>', ,name='orden_detalle'),
+    path('',TemplateView.as_view(template_name='base.html'))
+    # path('solicitud/crear', ,name='solicitud_crear'),
+    # path('solicitud/listar', ,name='solicitud_listar'),
+    # path('solicitud/detalle/<int:pk>', ,name='solicitud_detalles'),
+    # 
+    # path('orden/crear', ,name='orden_crear'),
+    # path('orden/listar', ,name='orden_listar'),
+    # path('orden/detalle/<int:pk>', ,name='orden_detalle'),
 ]
