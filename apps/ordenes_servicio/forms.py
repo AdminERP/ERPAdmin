364d1a8bf5078257fe9407ee80bce78656e105f9
Luis <luis.granja@correounivalle.edu.co>
2019-03-07 15:10:05 -0500

    updating

--- apps/ordenes_servicio/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from apps.ordenes_servicio.models import *
-
-class OrdenServicioForm(forms.ModelForm):
-    class Meta:
-        model = OrdenServicio
-        fields = ('servicio_vendido', 'encargado', 'cliente', 'comentarios')
--- apps/ordenes_servicio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.ordenes_servicio.views import *
-
-app_name = 'ordenes_servicio'
-
-urlpatterns = [
-    path('crear-orden-servicio', crear_orden_servicio, name='crear_orden_servicio'),
-]
