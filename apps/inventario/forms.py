a60eb631963c848001254f952cfdd4ae86e6ae8e
Sebastian Villegas <jhoan.villegas@correounivalle.edu.co>
2019-03-07 17:45:23 -0500

    pequeños cambios en crear entrada

--- apps/inventario/forms.py
+++ apps/inventario/forms.py
@@ -5,14 +5,9 @@ class RegistroEntrada(forms.ModelForm):
 
 	class Meta:
 		model = Entrada
-		fields = {"condicion", "razon_devolucion", "fecha", "ordenCompra"}
+		fields = {"condicion", "razon_devolucion", "ordenCompra"}
 		labels = {
 			'condicion': 'Condición de llegada',
             'razon_devolucion': 'Razón de devolución',
-            'fecha': 'Fecha de entrada',
             'ordenCompra': 'Orden de compra',
         }
-		widgets = {
-			'fecha': forms.DateInput(attrs={'class':'datepicker'}),
-		}
-
--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+CONDICIONES = (
+    (True, 'Buena'),
+    (False, 'Mala')
+)
+
 class OrdenCompra(models.Model):
     articulo = models.CharField(max_length=100)
     precio = models.CharField(max_length=100)
@@ -17,14 +22,32 @@ class OrdenCompra(models.Model):
     def listar():
         try:
             ordenes = OrdenCompra.objects.all()
+            #entradas = Entrada.objects.all()
+            #listaOrdenes = []
+            #for entrada in entradas:
+            #    listaOrdenes.append(entrada.ordenCompra.id)
+
+            #print(listaOrdenes)
+
+            #for orden in listaOrdenes
+
+            #print(entradas)
+            return ordenes
+        except OrdenCompra.DoesNotExist:
+            return None
+
+    @staticmethod
+    def listarActivos():
+        try:
+            ordenes = OrdenCompra.objects.all()
             return ordenes
         except OrdenCompra.DoesNotExist:
             return None
 
 class Entrada(models.Model):
-    condicion = models.BooleanField(default= True, null=False)
+    condicion = models.BooleanField(choices = CONDICIONES, default = 'Buena', null=False)
     razon_devolucion = models.CharField(max_length=500, null=True)
-    fecha = models.DateField()
+    fecha = models.DateField(auto_now = True)
     ordenCompra = models.ForeignKey(OrdenCompra, null=False, blank=False, on_delete=models.CASCADE)
 
 class Inventario(models.Model):
@@ -43,5 +66,3 @@ class Inventario(models.Model):
             return inventario
         except Inventario.DoesNotExist:
             return None
-
-
