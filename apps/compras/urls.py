928918082b39f4f9a492ba0f48eba6994dc99686
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2019-03-07 14:11:11 -0500

    index

--- apps/compras/urls.py
+++ apps/compras/urls.py
@@ -6,8 +6,9 @@ from django.views.generic.base import TemplateView
 from .views import ListarSolicitudes
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='base.html')),
+    path('',TemplateView.as_view(template_name='index_compras.html')),
     # path('solicitud/crear', ,name='solicitud_crear'),
+    path('solicitud/', ListarSolicitudes.as_view(), name='solicitud'),
     path('solicitud/listar', ListarSolicitudes.as_view(), name='solicitud_listar'),
     # path('solicitud/detalle/<int:pk>', ,name='solicitud_detalles'),
     # 
