d189eb94d5bef9462ed496156a0ca0a167aaf4c0
dbueno96 <daniel.bueno@correounivalle.edu.co>
2019-03-08 11:52:08 -0500

    vistas como clases

--- apps/compras/views.py
+++ apps/compras/views.py
@@ -1,40 +1,85 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from apps.compras.forms import OrdenCompraForm, SolicitudCompraForm, CotizacionForm
+from apps.compras.models import Cotizacion, SolicitudCompra, OrdenCompra
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView   
 # Create your views here.
 
 
 
-def cotizacion_create(request): 
-    if request.method == 'POST': 
-        form = CotizacionForm(request.POST) 
-        if form.is_valid(): 
-            form.save()
-        #return redirect (index )
-
-    else: 
-        form = CotizacionForm()
-    #return render (request, '/compras/cotizacion_form.html', {'form': form})
+class Prueba(CreateView): ##esta sirve solo para hacer pruebas 
+    model = Cotizacion
+    form_class = CotizacionForm
+    template_name= 'compras/prueba.html'
+    success_url = '/admin'
 
 
-def solicitud_create(request): 
-    if request.method == 'POST': 
-        form = SolicitudForm(request.POST) 
-        if form.is_valid(): 
-            form.save()
-        #return redirect (index )
+######---CREATES---######
+class CotizacionCreate(CreateView): 
+    model = Cotizacion 
+    form_class = CotizacionForm
+    #template_name = 
+    success_url = '/'   
 
-    else: 
-        form = SolicitudForm()
-    #return render (request, '/compras/solicitud_form.html', {'form': form})
+class SolicitudCreate(CreateView): 
+    model= SolicitudCompra
+    form_class= SolicitudCompraForm
+    #template_name= 
+    success_url = '/'
 
-def orden_create(request): 
-    if request.method == 'POST': 
-        form = OrdenForm(request.POST) 
-        if form.is_valid(): 
-            form.save()
-        #return redirect (index )
+class OrdenCreate(CreateView): 
+    model = OrdenCompra
+    form_class= OrdenCompraForm
+    #template_name = 
+    success_url= '/'
 
-    else: 
-        form = OrdenForm()
-    #return render (request, '/compras/solicitud_form.html', {'form': form})
 
+######---CONSULTAS---######
+class CotizacionList(ListView) : 
+    model = Cotizacion 
+    template_name= 'compras/prueba.html'
+
+class SolicitudList(ListView) : 
+    model = SolicitudCompra 
+    template_name= 'compras/prueba.html'
+
+class OrdenList(ListView) : 
+    model = OrdenCompra 
+    template_name= 'compras/prueba.html'
+
+
+#######---UPDATES---#######
+class CotizacionUpdate(UpdateView): 
+    model = Cotizacion 
+    form_class = CotizacionForm
+    #template_name = 
+    success_url = '/'   
+
+class SolicitudUpdate(UpdateView): 
+    model= SolicitudCompra
+    form_class= SolicitudCompraForm
+    #template_name= 
+    success_url = '/'
+
+class OrdenUpdate(UpdateView): 
+    model = OrdenCompra
+    form_class= OrdenCompraForm
+    #template_name = 
+    success_url= '/'
+
+
+######---DELETES---######
+class CotizacionDelete(DeleteView): 
+    model = Cotizacion 
+    #template_name = 
+    success_url = '/'   
+
+class SolicitudDelete(DeleteView): 
+    model= SolicitudCompra
+    #template_name= 
+    success_url = '/'
+
+class OrdenDelete(DeleteView): 
+    model = OrdenCompra
+    #template_name = 
+    success_url= '/'
