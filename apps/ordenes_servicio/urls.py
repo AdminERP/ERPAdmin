770fbf24393329eccb3aae09d3fdbbf2d0f7daf7
Diana GarCo <garcia.diana@correounivalle.edu.co>
2019-03-09 22:28:57 -0500

    urls

--- apps/ordenes_servicio/urls.py
+++ apps/ordenes_servicio/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('crear_cliente/',crear_cliente,name="Crear Cliente"),
     path('consultar_clientes/',consultar_clientes,name="Consultar Clientes"),
     path('crear_orden_servicio/', crear_orden_servicio, name='crear_orden_servicio'),
+    path('consultar-orden_servicio/', consultar_orden_servicio, name='consultar_orden_servicio'),
+    path('cancelar_orden_servicio/<int:id>', cancelar_orden_servicio, name='cancelar_orden_servicio'),
     path('operadores-autocomplete', operadores_autocomplete, name='operadores_autocomplete'),
     path('clientes-autocomplete', clientes_autocomplete, name='clientes_autocomplete'),
 ]
