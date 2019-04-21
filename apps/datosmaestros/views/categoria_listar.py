502b9a9c23cfbb77b104b2b11398cc6605ec60b5
Iván Toro <ivanmtoroc@gmail.com>
2019-04-21 14:42:46 -0500

    feat: relacionar cargo con categoria

--- apps/datosmaestros/views/categoria_listar.py
+++ apps/datosmaestros/views/categoria_listar.py
@@ -1,6 +1,7 @@
 # Django
 from django.views.generic import ListView
 
+
 # App Models
 from apps.datosmaestros.models import CategoriaModel
 
@@ -9,3 +10,11 @@ class CategoriaListView(ListView):
     model = CategoriaModel
     context_object_name = 'categorias'
     template_name = 'datosmaestros/categoria_listar.html'
+
+    def get_queryset(self):
+        queryset = super(ListView, self).get_queryset()
+        if not self.request.user.is_superuser:
+            queryset = CategoriaModel.objects.filter(
+                administrador = self.request.user.cargo
+            )
+        return queryset
